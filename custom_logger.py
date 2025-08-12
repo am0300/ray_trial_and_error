37733a21751ad61c77cf92f3db4aa6dba7913d12
am0300 <mayutaku2847@gmail.com>
2025-08-12 11:27:24 +0900

    ruff

--- custom_logger.py
+++ custom_logger.py
@@ -58,7 +58,7 @@ from ray.rllib.utils.metrics import (
     EPISODE_RETURN_MEAN,
     LEARNER_RESULTS,
 )
-from ray.tune.logger import Logger, LegacyLoggerCallback
+from ray.tune.logger import LegacyLoggerCallback, Logger
 
 
 class MyPrintLogger(Logger):
@@ -117,7 +117,7 @@ if __name__ == "__main__":
                 # Optional: Custom logdir (do not define this here
                 # for using ~/ray_results/...).
                 # "logdir": "/somewhere/on/my/file/system/"
-            }
+            },
         )
     )
 
